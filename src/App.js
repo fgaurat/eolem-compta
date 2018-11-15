6affba40eeeaf98938604a24df2459e7de599b8b
Frédéric Gaurat <fgaurat@eolem.com>
2018-11-15 09:59:48 +0100

    firest commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <>
+      <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Eolem</a>
+        <ul class="navbar-nav px-3">
+          <li class="nav-item text-nowrap">
+            
+          </li>
+        </ul>
+      </nav>
+
+      <div className="container-fluid">
+        
       </div>
+      </>
     );
   }
 }
